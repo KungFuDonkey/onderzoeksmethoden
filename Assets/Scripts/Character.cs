6bd4ad118d9f2b06d9d7ee5663eed4e8d6edbd7c
Lucas <l.k.disseldorp@students.uu.nl>
2021-02-28 14:19:31 +0100

    Added infection and minor building support

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    Building Home;
+    bool infected;
+    Building currentBuilding;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class Character : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ChooseBuilding(Building nextBuilding)
+    {
+        currentBuilding = nextBuilding;
     }
 }
