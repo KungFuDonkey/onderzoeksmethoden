302e1eef63f426219eddf616cc3f651f087c33cf
Lucas <l.k.disseldorp@students.uu.nl>
2021-02-28 15:09:19 +0100

    Added manual infection

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,17 +7,23 @@ public class Character : MonoBehaviour
     Building Home;
     bool infected = false;
     Building currentBuilding;
+    public Character toInfect;
+    Material mat;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mat = this.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            toInfect.getInfected();
+        }
     }
 
     public void ChooseBuilding(Building nextBuilding)
@@ -27,6 +33,8 @@ public class Character : MonoBehaviour
 
     public void getInfected()
     {
+        Debug.Log("infected");
         infected = true;
+        mat.color = Color.green;
     }
 }
